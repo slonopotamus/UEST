2a170761b631dd5a607075f088b8f796419e8453
Marat Radchenko <marat@slonopotamus.org>
2024-08-22 11:33:40 +0300

    Generify FieldGetter so it works with any type

--- Source/UEST/Private/UESTGameInstance.cpp
+++ Source/UEST/Private/UESTGameInstance.cpp
@@ -4,16 +4,14 @@
 // UGameInstance::WorldContext is not public, and neither InitializeStandalone nor InitializeForPlayInEditor is suitable for us.
 // So we use this template hack to access WorldContext field.
 // Source: https://ledas.com/post/857-how-to-hack-c-with-templates-and-friends/
-template<FWorldContext* UGameInstance::*WorldContext>
-struct Stealer
-{
-	static friend FWorldContext*& FieldGetter(UGameInstance& GameInstance)
-	{
-		return GameInstance.*WorldContext;
+template<typename T, auto T::* Field, typename RetVal>
+struct Stealer {
+	static friend RetVal& FieldGetter(T& Object) {
+		return Object.*Field;
 	}
 };
 
-template struct Stealer<&UGameInstance::WorldContext>;
+template struct Stealer<UGameInstance, &UGameInstance::WorldContext, FWorldContext*>;
 static FWorldContext*& FieldGetter(UGameInstance&);
 
 void IUESTGameInstance::DefaultInitializeForTests(UGameInstance& GameInstance, const bool bRunAsDedicated, const int32 PIEInstance)
