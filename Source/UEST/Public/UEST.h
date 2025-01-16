d00ca35c5879a18c50830ea4312ad1056359db27
Marat Radchenko <marat@slonopotamus.org>
2025-01-16 16:44:47 +0300

    Fix clang compilation for dependent template types

--- Source/UEST/Public/UEST.h
+++ Source/UEST/Public/UEST.h
@@ -418,7 +418,7 @@ namespace Is
 		const auto& _M = Matcher; \
 		/* TODO: Can we make this const auto& ? */ \
 		const auto _V = Value; \
-		if (!ensureAlwaysMsgf(_M.Matches<decltype(_V)>(_V), TEXT("%s: %s must %s"), TEXT(#Value), *CQTestConvert::ToString(_V), *_M.Describe())) \
+		if (!ensureAlwaysMsgf(_M.template Matches<decltype(_V)>(_V), TEXT("%s: %s must %s"), TEXT(#Value), *CQTestConvert::ToString(_V), *_M.Describe())) \
 		{ \
 			return; \
 		} \
