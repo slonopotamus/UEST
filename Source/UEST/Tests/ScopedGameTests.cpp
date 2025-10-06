16684c4d4c425cfbdc0b909bb03cd15ca4dcd67b
Marat Radchenko <marat@slonopotamus.org>
2025-10-06 20:02:15 +0300

    Fix comment

--- Source/UEST/Tests/ScopedGameTests.cpp
+++ Source/UEST/Tests/ScopedGameTests.cpp
@@ -1,8 +1,8 @@
 #include "ScopedGame.h"
 #include "UESTHelpers.h"
-// UEST.h needs to be after UESTHelpers.h
 #include "GameFramework/GameModeBase.h"
 #include "GameFramework/GameSession.h"
+// UEST.h needs to be after UESTHelpers.h
 #include "UEST.h"
 
 TEST(UEST, ScopedGame, Simple)
